daf7859f8dbf5774188e3042209aa7bc2da2d603
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-09-04 19:15:04 +0900

    ファイルを分割

---
+++ GrepFuncProtoType/GrepFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrepFuncProtoType
+{
+    public class GrepFile
+    {
+        public static void DirWalk(string targetDir)
+        {
+            try
+            {
+                foreach (string file in Directory.GetFiles(targetDir))
+                {
+                    Console.WriteLine(file);
+                }
+                foreach (string dir in Directory.GetDirectories(targetDir))
+                {
+                    DirWalk(dir);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
--- GrepFuncProtoType/Program.cs
+++ GrepFuncProtoType/Program.cs
@@ -9,27 +9,9 @@ namespace GrepFuncProtoType
 {
     class Program
     {
-        static void DirWalk(string targetDir)
-        {
-            try
-            {
-                foreach (string file in Directory.GetFiles(targetDir))
-                {
-                    Console.WriteLine(file);
-                }
-                foreach (string dir in Directory.GetDirectories(targetDir))
-                {
-                    DirWalk(dir);
-                }
-            }
-            catch (System.Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
         static void Main(string[] args)
         {
-            DirWalk(@".");
+            GrepFile.DirWalk(@".");
         }
     }
 }
