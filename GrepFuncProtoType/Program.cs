eb97f26b67964a6c63beb872f89e44a244ba0271
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-09-04 19:02:21 +0900

    再帰的にフォルダを検索する処理を実装

--- GrepFuncProtoType/Program.cs
+++ GrepFuncProtoType/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,27 @@ namespace GrepFuncProtoType
 {
     class Program
     {
+        static void DirWalk(string targetDir)
+        {
+            try
+            {
+                foreach (string file in Directory.GetFiles(targetDir))
+                {
+                    Console.WriteLine(file);
+                }
+                foreach (string dir in Directory.GetDirectories(targetDir))
+                {
+                    DirWalk(dir);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
         static void Main(string[] args)
         {
+            DirWalk(@".");
         }
     }
 }
