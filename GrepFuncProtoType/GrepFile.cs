c5385f036a9d746d9e6ce14d79ff0fc4b748ef2c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-09-09 20:22:37 +0900

    '(' ～ ')' までの間のデータを取り出してからパラメータの解析をするようにする

--- GrepFuncProtoType/GrepFile.cs
+++ GrepFuncProtoType/GrepFile.cs
@@ -13,14 +13,16 @@ namespace GrepFuncProtoType
     /// </summary>
     public class GrepFile
     {
-        const string strReturn    = @"(?<return>\w+)";
+        const string strReturn    = @"\b(?<return>\w+)\b";
         const string strFunc      = @"(?<func>[A-Za-z]\w*)";  // 先頭はアルファベット
         const string strType      = @"(?<type>[A-Za-z]\w*)";  // 先頭はアルファベット
-        const string strConst     = @"(const\s+)?";
+        const string strConst     = @"(\bconst\b)?";
         const string strAnd       = @"&";
         const string strPointer   = @"\*";
+        const string strPointerOption = @"\*?";
         const string strAndOrPtr  = @"\s*(" + strAnd + @"|" + strPointer + @")*\s*";
         const string strParam     = strConst + strType + strAndOrPtr;
+        const string strParams    = @"(?<params>[^\)]+)";
         const string strLeftPar   = @"\(";
         const string strRightPar  = @"\)";
         const string strSemicolon = @";";
@@ -38,15 +40,19 @@ namespace GrepFuncProtoType
 
         static readonly string[] elements = new string[] {
             "^",
-            strReturn + strSpace,
+            strConst,
+            strPointerOption,
+            strReturn,
+            strPointerOption,
             strFunc,
             strLeftPar,
-            strParam,
-            "(,",
-            strParam,
-            ")*",
+            strParams,
             strRightPar,
+            strConst,
             strSemicolon,
+        };
+        static readonly char[] comma = new char[] {
+            ','
         };
 
         /// <summary>
@@ -58,6 +64,44 @@ namespace GrepFuncProtoType
         /// Grep() で使用する正規表現クラス
         /// </summary>
         static Regex regPrototype = new Regex(strString);
+
+        /// <summary>
+        /// パラメータの要素を分解する正規表現クラス
+        /// </summary>
+        static Regex regParams = new Regex(@"\s+");
+
+        /// <summary>
+        /// 関数の引数の '(' ～ ')' までの間のデータを解析する
+        /// </summary>
+        /// <param name="parameters">'(' ～ ')' までの間のデータ</param>
+        /// <returns>対象とするデータの場合 true を返す</returns>
+        public static bool CheckParams(string parameters)
+        {
+            // 先頭、末尾の空白を削除する
+            string clippedParameters = parameters.Trim();
+
+            // カンマで分離する
+            string[] parametersElement = clippedParameters.Split(comma);
+
+            // void 関数の場合は問題ない
+            if (parametersElement.Length == 1 && parametersElement[0].Trim() == "void")
+            {
+                return false;
+            }
+
+            // 分離したパラメータごとに処理する
+            foreach(string parameter in parametersElement)
+            {
+                // パラメータの要素を分離して解析する
+                string[] elements = regParams.Split(parameter.Trim());
+                if (elements.Length == 1)
+                {
+                    // 要素数が一つの場合、タイプはないはず。でも完全ではない。
+                    return true;
+                }
+           }
+            return false;
+        }
 
         /// <summary>
         /// 指定したファイルからプロトタイプ宣言で型名しかない箇所を標準出力に出す。入力ファイルは UTF-8 と仮定する
@@ -77,8 +121,8 @@ namespace GrepFuncProtoType
                     if (match.Success)
                     {
                         string valReturn = match.Groups["return"].Value;
-                        string valFunc   = match.Groups["func"].Value;
-                        string valParam  = match.Groups["param"].Value;
+                        string valFunc = match.Groups["func"].Value;
+                        string valParams = match.Groups["params"].Value;
 
                         if (Array.IndexOf(keywords, valReturn) >= 0)
                         {
@@ -88,13 +132,13 @@ namespace GrepFuncProtoType
                         {
                             continue;
                         }
-                        if (Array.IndexOf(keywords, valParam) >= 0)
+
+                        if (!CheckParams(valParams))
                         {
-                            continue;
-                        }
-                        if (valParam == "void")
-                        {
-                            // 関数の引数が void の場合は問題ない
+#if DEBUG
+                            // うまく動かなかった場合のデバッグ用
+                            CheckParams(valParams);
+#endif
                             continue;
                         }
                         Console.WriteLine(fileName + "(" + lineNumber.ToString() + ")" + ":" + match.Groups[0].Value);
